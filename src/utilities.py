aeee2d09ef040ffc83e69709a25b9e185739fc9e
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2023-06-24 18:41:50 +0900

    add data transform

--- src/utilities.py
+++ src/utilities.py
@@ -5,9 +5,12 @@ from typing import Any, Callable, Literal, Optional, TypeAlias
 import numpy as np
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import transforms
+
+# First Party Library
+from src import Transforms, TransformsName, TransformsNameValue
 
 Model: TypeAlias = nn.Module
 Tensor: TypeAlias = torch.Tensor
@@ -15,6 +18,19 @@ Device: TypeAlias = Literal["cpu", "cuda"] | torch.device
 
 
 def weight_init(m: Any) -> None:
+    """weight initializer
+
+
+    Parameters
+    ----------
+    m : Any
+        Layer of model
+
+    Examples:
+    >>> model = Network()
+    >>> model.apply(weight_init)
+
+    """
     if isinstance(m, nn.Linear):
         torch.nn.init.kaiming_normal_(m.weight)
         m.bias.data.fill_(0.0)
@@ -26,43 +42,75 @@ def weight_init(m: Any) -> None:
 
 def get_dataloader(
     dataset_path: str,
-    batch_size: int,
-    running_mode: Literal["train", "pred"],
-) -> DataLoader:
+    dataset_transform: TransformsNameValue,
+    batch_size: int = 64,
+    shuffle: bool = True,
+    split_ratio: tuple[float, float] | None = None,
+    generator_seed: int | None = 42,
+) -> tuple[DataLoader, DataLoader] | DataLoader:
+    """specifying parameters and output dataloader
+
+    Parameters
+    ----------
+    dataset_path : str
+        Path to dataset
+    dataset_transform : TransformsNameValue
+        dictionary type parameters. {"transform": parameter}
+    batch_size : int, optional
+        Unit of data used per study, by default 64
+    shuffle : bool
+        The dataloader is shuffled if True, by default True
+    split_ratio : tuple[float, float] | None, optional
+        Split training and validation if not None, by default None
+
+    Returns
+    -------
+    DataLoader
+        _description_
+    """
     transform = transforms.Compose(
         [
-            transforms.Grayscale(1),
-            transforms.RandomHorizontalFlip(0.5),
-            transforms.RandomVerticalFlip(0.5),
-            transforms.ToTensor(),
+            str2transform(name, value)
+            for name, value in dataset_transform.items()
         ]
     )
+
     dataset = ImageFolder(dataset_path, transform)
-    return DataLoader(
-        dataset,
-        batch_size,
-        shuffle=True if running_mode == "train" else False,
-        num_workers=2,
-    )
+
+    if split_ratio is not None:
+        train_dataset, val_dataset = random_split(
+            dataset,
+            split_ratio,
+            generator=torch.Generator().manual_seed(generator_seed)
+            if generator_seed is not None
+            else None,
+        )
+        return (
+            DataLoader(train_dataset, batch_size, shuffle, num_workers=2),
+            DataLoader(val_dataset, batch_size, shuffle, num_workers=2),
+        )
+    else:
+        return DataLoader(dataset, batch_size, shuffle, num_workers=2)
 
 
-def train_step(
-    input_data: Tensor,
-    model: Model,
-    loss_func: Callable[[Tensor, Tensor], Tensor],
-    optimizer: torch.optim.Optimizer,
-    device: Device,
-) -> tuple[Tensor, Tensor]:
-    input_data = input_data.to(device)
-
-    x_pred = model(input_data)
-    loss, x_pred = loss_func(x_pred, input_data)
-
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-    return loss, x_pred
+def str2transform(
+    transforms_name: TransformsName,
+    transforms_param: Any,
+) -> Transforms:
+    if transforms_name == "Grayscale":
+        return transforms.Grayscale(transforms_param)
+    elif transforms_name == "RandomVerticalFlip":
+        return transforms.RandomVerticalFlip(transforms_param)
+    elif transforms_name == "RandomHorizontalFlip":
+        return transforms.RandomHorizontalFlip(transforms_param)
+    elif transforms_name == "Normalize":
+        return transforms.Normalize(transforms_param[0], transforms_param[1])
+    elif transforms_name == "ToTensor":
+        return transforms.ToTensor()
+    else:
+        raise RuntimeError(
+            f'There is no torchvision.transforms such as "{transforms_name}"'
+        )
 
 
 def calc_loss(

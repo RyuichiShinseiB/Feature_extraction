68feb61deb3864cdc7091fb3592e7e27429b1bfe
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2023-06-24 18:41:17 +0900

    add some typealias

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,43 @@ from typing import Literal, TypeAlias
 
 # Third Party Library
 import torch
+from torchvision import transforms
+
+Model: TypeAlias = torch.nn.Module
+Device: TypeAlias = Literal["cpu", "cuda"] | torch.device
+Tensor: TypeAlias = torch.Tensor
+ModelName = Literal[
+    "SECAE32",
+    "SECAE64",
+    "SECVAE64",
+    "SECVAEsoftplus64",
+    "SimpleCAE32",
+    "SimpleCAE64",
+    "SimpleCAE128",
+    "SimpleCVAE64",
+    "SimpleCVAEsoftplus64",
+]
 
 ActivationName = Literal[
     "relu", "selu", "leakyrelu", "sigmoid", "tanh", "identity"
 ]
-Device: TypeAlias = Literal["cpu", "cuda"] | torch.device
-Tensor: TypeAlias = torch.Tensor
+
+TransformsName = Literal[
+    "Grayscale",
+    "RandomHorizontalFlip",
+    "RandomVerticalFlip",
+    "Normalize",
+    "ToTensor",
+]
+
+TransformsNameValue = dict[
+    TransformsName, int | float | tuple[float, float] | None
+]
+
+Transforms: TypeAlias = (
+    transforms.Grayscale
+    | transforms.RandomVerticalFlip
+    | transforms.RandomHorizontalFlip
+    | transforms.Normalize
+    | transforms.ToTensor
+)

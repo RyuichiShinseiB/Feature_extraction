79281a7272a0992e46cd3f35765c3967dc504202
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2023-06-29 21:44:37 +0900

    summarize loss functions

---
+++ src/loss_function.py
@@ -0,0 +1,70 @@
+# Standard Library
+from typing import Callable, Literal
+
+# Third Party Library
+import torch
+
+# Local Library
+from . import Model, Tensor
+
+
+def calc_loss(
+    input_data: Tensor,
+    reconst_loss: Callable[[Tensor, Tensor], Tensor],
+    latent_loss: Callable[[Tensor, Tensor], Tensor] | None,
+    model: Model,
+) -> tuple[Tensor, Tensor]:
+    x_pred, _ = model(input_data)
+    loss = reconst_loss(x_pred, input_data)
+    if latent_loss:
+        loss += latent_loss
+    return loss, x_pred
+
+
+class LossFunction:
+    def __init__(
+        self,
+        reconst_loss_type: Literal["bce", "mse"],
+        var_calc_type: Literal["softplus", "general"] | None,
+    ) -> None:
+        self.reconst: torch.nn.BCELoss | torch.nn.MSELoss
+        self.latent: LatentLoss | None
+
+        if reconst_loss_type == "bce":
+            self.reconst = torch.nn.BCELoss()
+        elif reconst_loss_type == "mse":
+            self.reconst = torch.nn.MSELoss()
+        else:
+            raise RuntimeError("Please select another loss function")
+
+        if var_calc_type:
+            self.latent = LatentLoss(var_calc_type)
+        else:
+            self.latent = None
+
+
+class LatentLoss:
+    def __init__(self, var_calc_type: Literal["softplus", "general"]) -> None:
+        if var_calc_type == "softplus":
+            self.latent_loss = self.softplus_latent
+        elif var_calc_type == "standard":
+            self.latent_loss = self.general_latent
+        pass
+
+    def __call__(self, mean: Tensor, var: Tensor) -> Tensor:
+        return self.latent_loss(mean, var)
+
+    @staticmethod
+    def softplus_latent(mean: Tensor, var: Tensor) -> Tensor:
+        eps = 1e-5
+        return -0.5 * torch.mean(
+            torch.sum(1 + torch.log(var + eps) - mean**2 - var, dim=1).view(
+                -1
+            )
+        )
+
+    @staticmethod
+    def general_latent(mean: Tensor, log_var: Tensor) -> Tensor:
+        return -0.5 * torch.mean(
+            torch.sum(1 + log_var - mean**2 - log_var.exp(), dim=1)
+        )

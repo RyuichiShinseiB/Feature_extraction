62a7e8cedf07b022c02549716392bf2d172dbb41
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2023-07-13 22:30:47 +0900

    add simpleCVAE32

---
+++ src/predefined_models/_SimpleConvVAE_softplus32.py
@@ -0,0 +1,158 @@
+# Third Party Library
+import torch
+from torch import nn
+
+# Local Library
+from .. import ActivationName, Device, Tensor
+from ._CNN_modules import DownShape, UpShape, add_activation
+
+
+class VariationalEncoder(nn.Module):
+    def __init__(
+        self,
+        input_channels: int = 1,
+        encoder_base_channels: int = 64,
+        latent_dimensions: int = 10,
+        activation: ActivationName = "relu",
+        output_activation: ActivationName = "relu",
+        device: Device = "cpu",
+    ) -> None:
+        super().__init__()
+        self.device = device
+
+        self.l1 = DownShape(input_channels, encoder_base_channels, activation)
+        self.l2 = DownShape(
+            encoder_base_channels, encoder_base_channels * 2, activation
+        )
+        self.l3 = DownShape(
+            encoder_base_channels * 2, encoder_base_channels * 4, activation
+        )
+        self.l4 = DownShape(
+            encoder_base_channels * 4, encoder_base_channels * 8, activation
+        )
+        self.l5 = DownShape(
+            encoder_base_channels * 8, encoder_base_channels * 16, activation
+        )
+
+        self.l_mean = nn.Sequential(
+            nn.Linear(
+                encoder_base_channels * 16, latent_dimensions, bias=False
+            ),
+            add_activation(output_activation),
+        )
+        self.l_var = nn.Sequential(
+            nn.Linear(
+                encoder_base_channels * 16, latent_dimensions, bias=False
+            ),
+            nn.Softplus(),
+        )
+
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+        h = self.l1(x)
+        h = self.l2(h)
+        h = self.l3(h)
+        h = self.l4(h)
+        h = self.l5(h)
+        h = torch.flatten(h, start_dim=1)
+        mean = self.l_mean(h)
+        var = self.l_var(h)
+
+        return mean, var
+
+
+######################################################
+class VariationalDecoder(nn.Module):
+    def __init__(
+        self,
+        latent_dimensions: int = 10,
+        decoder_base_channels: int = 64,
+        input_channels: int = 1,
+        activation: ActivationName = "relu",
+        output_activation: ActivationName = "sigmoid",
+        device: Device = "cpu",
+    ) -> None:
+        super().__init__()
+        self.device = device
+
+        self.l1 = UpShape(
+            latent_dimensions, decoder_base_channels * 16, activation
+        )
+        self.l2 = UpShape(
+            decoder_base_channels * 16, decoder_base_channels * 8, activation
+        )
+        self.l3 = UpShape(
+            decoder_base_channels * 8, decoder_base_channels * 4, activation
+        )
+        self.l4 = UpShape(
+            decoder_base_channels * 4, decoder_base_channels * 2, activation
+        )
+        self.l5 = UpShape(
+            decoder_base_channels * 2, input_channels, output_activation
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.l1(x)
+        x = self.l2(x)
+        x = self.l3(x)
+        x = self.l4(x)
+        x = self.l5(x)
+        return x
+
+
+###################################################################
+class SimpleCVAEsoftplus32(nn.Module):
+    def __init__(
+        self,
+        input_channels: int = 1,
+        latent_dimensions: int = 10,
+        encoder_base_channels: int = 64,
+        decoder_base_channels: int = 64,
+        encoder_activation: ActivationName = "relu",
+        decoder_activation: ActivationName = "relu",
+        encoder_output_activation: ActivationName = "sigmoid",
+        decoder_output_activation: ActivationName = "tanh",
+        device: Device = "cpu",
+    ) -> None:
+        super().__init__()
+        self.device = device
+        self.latent_dimensions = latent_dimensions
+        self.encoder = VariationalEncoder(
+            input_channels,
+            encoder_base_channels,
+            latent_dimensions,
+            encoder_activation,
+            encoder_output_activation,
+            device,
+        )
+        self.decoder = VariationalDecoder(
+            latent_dimensions,
+            decoder_base_channels,
+            input_channels,
+            decoder_activation,
+            decoder_output_activation,
+            device,
+        )
+
+    def forward(
+        self, x: Tensor
+    ) -> tuple[Tensor, tuple[Tensor, Tensor, Tensor]]:
+        mean, var = self.encoder(x)
+        z = self.reparameterization(mean, var)
+        x_pred = self.decoder(z)
+
+        return x_pred, (z, mean, var)
+
+    def reparameterization(self, mean: Tensor, var: Tensor) -> Tensor:
+        eps = torch.randn(mean.shape, device=self.device)
+        return (mean + eps * torch.sqrt(var)).view(
+            mean.shape[0], self.latent_dimensions, 1, 1
+        )
+
+
+if __name__ == "__main__":
+    # Third Party Library
+    from torchinfo import summary
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SimpleCVAEsoftplus32(device=device)
+    summary(model, (1, 1, 32, 32))
--- src/predefined_models/__init__.py
+++ src/predefined_models/__init__.py
@@ -6,10 +6,10 @@ from typing import Any
 from omegaconf import DictConfig, OmegaConf
 
 # First Party Library
-from src.configs.model_configs import ModelConfig
+from src.configs.model_configs import AutoencoderModelConfig, MAEViTModelConfig
 
 # Local Library
-from .. import Device, Model
+from .. import Device, Model, ModelName
 from ._SECAE32 import SECAE32
 from ._SECAE64 import SECAE64
 from ._SEConvVAE64 import SECVAE64
@@ -18,24 +18,28 @@ from ._SimpleCAE32 import SimpleCAE32
 from ._SimpleCAE64 import SimpleCAE64
 from ._SimpleCAE128 import SimpleCAE128
 from ._SimpleConvVAE64 import SimpleCVAE64
+from ._SimpleConvVAE_softplus32 import SimpleCVAEsoftplus32
 from ._SimpleConvVAE_softplus64 import SimpleCVAEsoftplus64
 
 
 def set_hyper_parameters(
-    model_cfg: ModelConfig | DictConfig,
+    model_cfg: AutoencoderModelConfig | MAEViTModelConfig,
 ) -> dict[str, int | str] | Any:
-    if isinstance(model_cfg, ModelConfig):
+    if isinstance(model_cfg, AutoencoderModelConfig | MAEViTModelConfig):
         return asdict(model_cfg.hyper_parameters)
     elif isinstance(model_cfg, DictConfig):
         return OmegaConf.to_container(model_cfg.hyper_parameters)
     else:
-        raise ValueError("model_cfg is not ModelConfig or DictConfig")
+        raise ValueError(
+            "model_cfg is not AutoencoderModelConfig, MAEViTModelConfig or DictConfig"
+        )
 
 
 def model_define(
-    model_cfg: ModelConfig | DictConfig, device: Device = "cpu"
+    model_cfg: AutoencoderModelConfig | MAEViTModelConfig,
+    device: Device = "cpu",
 ) -> Model:
-    model_name = model_cfg.name
+    model_name: ModelName = model_cfg.name
     hyper_parameters = set_hyper_parameters(model_cfg)
 
     if model_name == "SECAE32":
@@ -54,6 +58,8 @@ def model_define(
         return SimpleCAE128(**hyper_parameters, device=device)
     elif model_name == "SimpleCVAE64":
         return SimpleCVAE64(**hyper_parameters, device=device)
+    elif model_name == "SimpleCVAE_softplus32":
+        return SimpleCVAEsoftplus32(**hyper_parameters, device=device)
     elif model_name == "SimpleCVAE_softplus64":
         return SimpleCVAEsoftplus64(**hyper_parameters, device=device)
     else:

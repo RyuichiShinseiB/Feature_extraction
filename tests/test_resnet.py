3e6cbd167eee66781a25513f6f302cb8a3e98479
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-16 15:31:16 +0900

    Added test for MyBottleneck

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -2,6 +2,7 @@ import torch
 
 from src.predefined_models._CNN_modules import (
     MyBasicBlock,
+    MyBottleneck,
     ResNet,
 )
 
@@ -15,6 +16,23 @@ def test_mybasicblock() -> None:
         out_ch=100,
         activation="relu",
     ).to(device)
+
+    t = torch.rand(10, 3, 32, 32, device=device)
+
+    out = resnet(t)
+    assert out.shape == (10, 100)
+
+
+def test_mybottleneck() -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    resnet = ResNet(
+        MyBottleneck,
+        layers=(3, 4, 6, 3),
+        in_ch=3,
+        out_ch=100,
+        activation="relu",
+    ).to(device)
+    print(resnet)
 
     t = torch.rand(10, 3, 32, 32, device=device)
 

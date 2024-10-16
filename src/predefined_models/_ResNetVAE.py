105e8645156a06b67e9e9bfb3e0492b9aba93038
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-16 16:08:50 +0900

    Defined ResNetEncoder

--- src/predefined_models/_ResNetVAE.py
+++ src/predefined_models/_ResNetVAE.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
-from ..mytyping import ActivationName, Device
-from ._CNN_modules import MyBasicBlock, ResNet
+from ..mytyping import ActivationName, Device, Tensor
+from ._CNN_modules import MyBasicBlock, ResNet, add_activation
 
 
 class ResNetEncoder(nn.Module):
@@ -11,15 +11,38 @@ class ResNetEncoder(nn.Module):
         encoder_base_channels: int = 64,
         latent_dimensions: int = 10,
         activation: ActivationName = "relu",
-        output_activation: ActivationName = "relu",
+        mean_layer_activation: ActivationName = "relu",
+        var_layer_activation: ActivationName = "relu",
         device: Device = "cpu",
     ) -> None:
         super().__init__()
+        self.device = device
         self.resnet = ResNet(
-            MyBasicBlock,
+            block=MyBasicBlock,
             layers=(3, 4, 6, 3),
             in_ch=input_channels,
             out_ch=latent_dimensions,
+            inplanes=encoder_base_channels,
             activation=activation,
         )
-        pass
+
+        self.mean_layer = nn.Sequential(
+            nn.Linear(latent_dimensions, latent_dimensions),
+            add_activation(activation),
+            nn.Linear(latent_dimensions, latent_dimensions),
+            add_activation(mean_layer_activation),
+        )
+
+        self.var_layer = nn.Sequential(
+            nn.Linear(latent_dimensions, latent_dimensions),
+            add_activation(activation),
+            nn.Linear(latent_dimensions, latent_dimensions),
+            add_activation(var_layer_activation),
+        )
+
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+        x = self.resnet(x)
+        mean = self.mean_layer(x)
+        var = self.var_layer(x)
+
+        return mean, var

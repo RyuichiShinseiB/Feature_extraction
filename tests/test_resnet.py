cd7826e0f6b2e6f478e34d314f575c2a37a42ad1
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-17 17:41:34 +0900

    Added test for resnet using sebottleneck

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -4,6 +4,7 @@ from src.predefined_models._CNN_modules import (
     MyBasicBlock,
     MyBottleneck,
     ResNet,
+    SEBottleneck,
 )
 
 
@@ -27,6 +28,23 @@ def test_mybottleneck() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     resnet = ResNet(
         MyBottleneck,
+        layers=(3, 4, 6, 3),
+        in_ch=3,
+        out_ch=100,
+        activation="relu",
+    ).to(device)
+    print(resnet)
+
+    t = torch.rand(10, 3, 32, 32, device=device)
+
+    out = resnet(t)
+    assert out.shape == (10, 100)
+
+
+def test_sebottleneck() -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    resnet = ResNet(
+        SEBottleneck,
         layers=(3, 4, 6, 3),
         in_ch=3,
         out_ch=100,

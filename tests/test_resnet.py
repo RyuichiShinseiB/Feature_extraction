5786078e13da6efeeb1f13a408606f6db9daa964
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-22 18:52:55 +0900

    Added test for resnet_

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -4,6 +4,7 @@ from src.predefined_models._CNN_modules import (
     MyBasicBlock,
     MyBottleneck,
     ResNet,
+    ResNet_,
     SEBottleneck,
 )
 
@@ -20,7 +21,8 @@ def test_mybasicblock() -> None:
 
     t = torch.rand(10, 3, 32, 32, device=device)
 
-    out = resnet(t)
+    out, indices = resnet(t)
+    print(f"{indices.shape=}")
     assert out.shape == (10, 100)
 
 
@@ -33,11 +35,11 @@ def test_mybottleneck() -> None:
         out_ch=100,
         activation="relu",
     ).to(device)
-    print(resnet)
 
     t = torch.rand(10, 3, 32, 32, device=device)
 
-    out = resnet(t)
+    out, indices = resnet(t)
+    print(f"{indices.shape=}")
     assert out.shape == (10, 100)
 
 
@@ -50,9 +52,26 @@ def test_sebottleneck() -> None:
         out_ch=100,
         activation="relu",
     ).to(device)
-    print(resnet)
 
     t = torch.rand(10, 3, 32, 32, device=device)
 
-    out = resnet(t)
+    out, indices = resnet(t)
     assert out.shape == (10, 100)
+
+
+def test_upsample_mybasicblock() -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    t = torch.rand(10, 100, device=device)
+    indices = torch.randint(0, 10, (10, 64, 8, 8), device=device)
+    resnet = ResNet_(
+        MyBasicBlock,
+        layers=(3, 4, 6, 3),
+        in_ch=100,
+        out_ch=1,
+        activation="relu",
+        reconstructed_size=(32, 32),
+    ).to(device)
+    print(resnet)
+
+    out = resnet(t, indices)
+    assert out.shape == (10, 1, 32, 32)

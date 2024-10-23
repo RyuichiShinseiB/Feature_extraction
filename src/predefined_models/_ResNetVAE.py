b8916acfc28539b806d6a49c5a3e39063961d6f1
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-23 16:27:35 +0900

    Moved UpSamplingResNet and DownSamplingResNet from _CNN_modules.py to _ResNetVAE.py

--- src/predefined_models/_ResNetVAE.py
+++ src/predefined_models/_ResNetVAE.py
@@ -1,48 +1,328 @@
-import torch.nn as nn
+from collections.abc import Callable
+
+import torch
+from torch import nn
 
 from ..mytyping import ActivationName, Device, Tensor
-from ._CNN_modules import MyBasicBlock, ResNet, add_activation
+from ._CNN_modules import (
+    MyBasicBlock,
+    MyBottleneck,
+    SEBottleneck,
+    _calc_layers_output_size,
+    _inplanes,
+    _make_resnet_layer,
+    add_activation,
+)
 
 
-class ResNetEncoder(nn.Module):
+class DownSamplingResNet(nn.Module):
     def __init__(
         self,
-        input_channels: int = 1,
-        encoder_base_channels: int = 64,
-        latent_dimensions: int = 10,
+        block: type[MyBasicBlock | MyBottleneck | SEBottleneck],
+        layers: tuple[int, int, int, int],
+        in_ch: int = 1,
+        out_ch: int = 1000,
+        inplanes: int = 64,
+        zero_init_residual: bool = False,
         activation: ActivationName = "relu",
-        mean_layer_activation: ActivationName = "relu",
-        var_layer_activation: ActivationName = "relu",
-        device: Device = "cpu",
+        norm_layer: Callable[..., nn.Module] | None = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.inplaneses = tuple(
+            [inplanes] + list(_inplanes(inplanes * 4, 2, True, 4))
+        )
+        self.conv1 = nn.Conv2d(
+            in_ch,
+            self.inplaneses[0],
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
+        )
+        self.bn1 = norm_layer(inplanes)
+        self.actfunc = add_activation(activation)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=3, stride=2, padding=1, return_indices=True
+        )
+        self.layer1 = _make_resnet_layer(
+            block,
+            self.inplaneses[0],
+            self.inplaneses[1],
+            layers[0],
+            activation=activation,
+        )
+        self.layer2 = _make_resnet_layer(
+            block,
+            self.inplaneses[1],
+            self.inplaneses[2],
+            layers[1],
+            stride=2,
+            activation=activation,
+        )
+        self.layer3 = _make_resnet_layer(
+            block,
+            self.inplaneses[2],
+            self.inplaneses[3],
+            layers[2],
+            stride=2,
+            activation=activation,
+        )
+        self.layer4 = _make_resnet_layer(
+            block,
+            self.inplaneses[3],
+            self.inplaneses[4],
+            layers[3],
+            stride=2,
+            activation=activation,
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(self.inplaneses[4], out_ch)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_out", nonlinearity="relu"
+                )
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.  # noqa: E501
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, MyBottleneck) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, MyBasicBlock) and m.bn2.weight is not None:
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _forward_impl(
+        self, x: Tensor
+    ) -> tuple[Tensor, tuple[int, ...], Tensor]:
+        x = self.conv1(x)
+        output_size_conv1 = x.shape
+        x = self.bn1(x)
+        x = self.actfunc(x)
+        x, pool_indices = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x, output_size_conv1, pool_indices
+
+    def forward(self, x: Tensor) -> tuple[Tensor, tuple[int, ...], Tensor]:
+        return self._forward_impl(x)
+
+
+class UpSamplingResNet(nn.Module):
+    def __init__(
+        self,
+        block: type[MyBasicBlock | MyBottleneck | SEBottleneck],
+        layers: tuple[int, int, int, int],
+        in_ch: int = 1000,
+        out_ch: int = 1,
+        inplanes: int = 64,
+        zero_init_residual: bool = False,
+        activation: ActivationName = "relu",
+        output_activation: ActivationName = "sigmoid",
+        norm_layer: Callable[..., nn.Module] | None = None,
+        *,
+        reconstructed_size: tuple[int, int] = (64, 64),
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        self.inplaneses = tuple(
+            [inplanes] + list(_inplanes(inplanes * 4, 2, True, 4))
+        )
+        self.reconstructed_size = reconstructed_size
+
+        self.output_actfunc = add_activation(output_activation)
+        self.conv1_t = nn.ConvTranspose2d(
+            self.inplaneses[0],
+            out_ch,
+            kernel_size=7,
+            stride=2,
+            padding=3,
+            bias=False,
+            output_padding=1,
+        )
+        self.bn1_t = norm_layer(self.inplaneses[0])
+        self.actfunc = add_activation(activation)
+        self.maxpool_t = nn.MaxUnpool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1_t = _make_resnet_layer(
+            block,
+            self.inplaneses[1],
+            self.inplaneses[0],
+            layers[0],
+            activation=activation,
+            how_sampling="up",
+        )
+        self.layer2_t = _make_resnet_layer(
+            block,
+            self.inplaneses[2],
+            self.inplaneses[1],
+            layers[1],
+            stride=2,
+            activation=activation,
+            how_sampling="up",
+        )
+        self.layer3_t = _make_resnet_layer(
+            block,
+            self.inplaneses[3],
+            self.inplaneses[2],
+            layers[2],
+            stride=2,
+            activation=activation,
+            how_sampling="up",
+        )
+        self.layer4_t = _make_resnet_layer(
+            block,
+            self.inplaneses[4],
+            self.inplaneses[3],
+            layers[3],
+            stride=2,
+            activation=activation,
+            how_sampling="up",
+        )
+
+        before_avgpool_size = _calc_layers_output_size(
+            reconstructed_size,
+            # (conv1, maxpool, layer1, layer2, layer3, layer4)
+            kernel_size=(7, 3, 3, 3, 3, 3),
+            padding=(3, 1, 1, 1, 1, 1),
+            dilation=(1, 1, 1, 1, 1, 1),
+            stride=(2, 2, 1, 2, 2, 2),
+        )
+        self.avgpool_t = nn.UpsamplingNearest2d(before_avgpool_size)
+        self.fc = nn.Linear(in_ch, self.inplaneses[4])
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_out", nonlinearity="relu"
+                )
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.  # noqa: E501
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, MyBottleneck) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, MyBasicBlock) and m.bn2.weight is not None:
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _forward_impl(
+        self,
+        x: Tensor,
+        input_size_conv1_t: tuple[int, ...],
+        pool_indices: Tensor,
+    ) -> Tensor:
+        x = self.fc(x)
+        x = self.actfunc(x)
+        x = self.avgpool_t(x.view(x.shape[0], -1, 1, 1))
+
+        x = self.layer4_t(x)
+        x = self.layer3_t(x)
+        x = self.layer2_t(x)
+        x = self.layer1_t(x)
+
+        x = self.maxpool_t(x, pool_indices, input_size_conv1_t)
+        x = self.actfunc(x)
+        x = self.bn1_t(x)
+        x = self.conv1_t(x)
+        x = self.output_actfunc(x)
+
+        return x
+
+    def forward(
+        self,
+        x: Tensor,
+        input_size_conv1_t: tuple[int, ...],
+        pool_indices: Tensor,
+    ) -> Tensor:
+        return self._forward_impl(x, input_size_conv1_t, pool_indices)
+
+
+###############################################################################
+class ResNetVAE(nn.Module):
+    def __init__(
+        self,
+        input_channels: int,
+        latent_dimensions: int,
+        encoder_base_channels: int,
+        decoder_base_channels: int,
+        encoder_activation: ActivationName,
+        decoder_activation: ActivationName,
+        encoder_output_activation: ActivationName,
+        decoder_output_activation: ActivationName,
+        input_size: tuple[int, int],
+        device: Device,
     ) -> None:
         super().__init__()
         self.device = device
-        self.resnet = ResNet(
+        self.latent_dimensions = latent_dimensions
+
+        self.encoder = DownSamplingResNet(
             block=MyBasicBlock,
             layers=(3, 4, 6, 3),
             in_ch=input_channels,
             out_ch=latent_dimensions,
             inplanes=encoder_base_channels,
-            activation=activation,
+            activation=encoder_activation,
         )
 
         self.mean_layer = nn.Sequential(
             nn.Linear(latent_dimensions, latent_dimensions),
-            add_activation(activation),
+            add_activation(encoder_activation),
             nn.Linear(latent_dimensions, latent_dimensions),
-            add_activation(mean_layer_activation),
+            add_activation(encoder_output_activation),
         )
-
         self.var_layer = nn.Sequential(
             nn.Linear(latent_dimensions, latent_dimensions),
-            add_activation(activation),
+            add_activation(encoder_activation),
             nn.Linear(latent_dimensions, latent_dimensions),
-            add_activation(var_layer_activation),
+            nn.Softplus(),
         )
 
-    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
-        x = self.resnet(x)
+        self.decoder = UpSamplingResNet(
+            block=MyBasicBlock,
+            layers=(3, 4, 6, 3),
+            in_ch=latent_dimensions,
+            out_ch=input_channels,
+            inplanes=decoder_base_channels,
+            activation=decoder_activation,
+            output_activation=decoder_output_activation,
+            reconstructed_size=input_size,
+        )
+
+    def forward(
+        self, x: Tensor
+    ) -> tuple[Tensor, tuple[Tensor, Tensor, Tensor]]:
+        x, output_size_conv1, indices = self.encoder.forward(x)
         mean = self.mean_layer(x)
         var = self.var_layer(x)
+        z = self.reparametarization(mean, var)
 
-        return mean, var
+        x = self.decoder.forward(z, output_size_conv1, indices)
+        return x, (z, mean, var)
+
+    @staticmethod
+    def reparametarization(mean: Tensor, var: Tensor) -> Tensor:
+        eps = torch.randn(mean.shape)
+        return mean + var.sqrt() * eps

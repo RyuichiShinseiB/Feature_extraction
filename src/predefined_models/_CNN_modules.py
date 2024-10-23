2b7e7b8eb5c66fc097c9a34aa7d40821a29c714f
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-10-23 16:22:37 +0900

    Deleted comment out lines

--- src/predefined_models/_CNN_modules.py
+++ src/predefined_models/_CNN_modules.py
@@ -614,19 +614,6 @@ class UpSamplingResNet(nn.Module):
         x = self.bn1_t(x)
         x = self.conv1_t(x)
         x = self.output_actfunc(x)
-        # x = self.conv1(x)
-        # x = self.bn1(x)
-        # x = self.actfunc(x)
-        # x = self.maxpool(x)
-
-        # x = self.layer1(x)
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-
-        # x = self.avgpool(x)
-        # x = torch.flatten(x, 1)
-        # x = self.fc(x)
 
         return x
 

0f921241719a0b90a204e044bdfaf92eee0254c3
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-11-14 09:00:19 +0900

    Changed method name and return value of __str__ method

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ from typing import Any
 class CatchSIGTERM(Exception):
     @staticmethod
     def __str__() -> str:
-        return "Exception: Catch SIGTERM"
+        return "Exception caused by catching SIGTERM"
 
     @staticmethod
     def __repr__() -> str:
@@ -16,5 +16,5 @@ class CatchSIGTERM(Exception):
         raise CatchSIGTERM
 
     @classmethod
-    def signal_handling(cls) -> None:
+    def start_signal_handling(cls) -> None:
         signal.signal(signal.SIGTERM, cls.handler)

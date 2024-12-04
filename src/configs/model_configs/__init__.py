03868b2310b00f3004415bc43aa158ca289dcee8
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-04 15:26:08 +0900

    Changed to collect as a module directory

---
+++ src/configs/model_configs/__init__.py
@@ -0,0 +1,9 @@
+from .autoencoder_configs import (
+    AutoencoderModelConfig,
+    ExtractConfig,
+    MAEViTModelConfig,
+    TrainAutoencoderConfig,
+    TrainMAEViTConfig,
+    dict2dataclass,
+    dictconfig2dataclass,
+)

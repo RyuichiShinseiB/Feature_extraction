c348d01a252d32b6901376a39238137b61b044c1
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-05 20:50:03 +0900

    Added test for converting config

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any
 
 from hydra import compose, initialize
 
@@ -18,3 +19,19 @@ def test_train_classification_model() -> None:
         cfg.model.feature.hyper_parameters.output_channels
         == cfg.model.classifier.hyper_parameters.input_dimension
     )
+
+
+def test_cvt_cfg_to_dict() -> None:
+    with initialize("../src/configs/train_conf/", version_base=None):
+        _cfg = compose("classification")
+
+    cfg = TrainClassificationModel.from_dictconfig(_cfg).to_dict()
+
+    def _assert_recursive_dict(d: dict[str, None | dict | Any]) -> None:
+        for v in d.values():
+            if v is None:
+                assert True
+            if isinstance(v, dict):
+                _assert_recursive_dict(v)
+
+    _assert_recursive_dict(cfg)

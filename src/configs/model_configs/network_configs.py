691085caa84744821d05cf4145789ed424bd24f1
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-05 19:36:54 +0900

    Created classifier model configuration

--- src/configs/model_configs/network_configs.py
+++ src/configs/model_configs/network_configs.py
@@ -1,8 +1,64 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
+from pathlib import Path
 
-from .autoencoder_configs import RecursiveDataclass
+from ...mytyping import ActivationName, ModelName, ResNetBlockName
+from .autoencoder_configs import (
+    RecursiveDataclass,
+    TrainConfig,
+    TrainDatasetConfig,
+)
+
+
+@dataclass
+class NetworkHyperParams(RecursiveDataclass):
+    # for mlp model
+    input_dimension: int | None = None
+    middle_dimensions: Sequence[int] | None = None
+    output_dimension: int | None = None
+    activation: ActivationName | None = None
+
+    # for cnn model
+    input_channels: int | None = None
+    middle_channels: int | None = None
+    output_channels: int | None = None
+
+    # for autoencoder model
+    latent_dimensions: int | None = None
+    encoder_base_channels: int | None = None
+    decoder_base_channels: int | None = None
+    encoder_activation: ActivationName | None = None
+    decoder_activation: ActivationName | None = None
+    encoder_output_activation: ActivationName | None = None
+    decoder_output_activation: ActivationName | None = None
+
+    # for resnet
+    block_name: ResNetBlockName | None = None
+    input_size: Sequence[int] | None = None
 
 
 @dataclass
 class NetworkConfig(RecursiveDataclass):
-    pass
+    network_type: ModelName = "MLP"
+    pretrained_path: Path | None = None
+    hyper_parameters: NetworkHyperParams = NetworkHyperParams()
+
+    def __post_init__(self) -> None:
+        if self.pretrained_path is None:
+            return
+        if not isinstance(self.pretrained_path, Path):
+            self.pretrained_path = Path(self.pretrained_path)
+
+
+@dataclass
+class ClassificationModelConfig(RecursiveDataclass):
+    name: str = "NeuralNetwork"
+    feature: NetworkConfig = NetworkConfig()
+    classifier: NetworkConfig = NetworkConfig()
+
+
+@dataclass
+class TrainClassificationModel(RecursiveDataclass):
+    model: ClassificationModelConfig = ClassificationModelConfig()
+    train: TrainConfig = TrainConfig()
+    dataset: TrainDatasetConfig = TrainDatasetConfig()

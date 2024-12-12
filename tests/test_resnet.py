cc41e14d1f552ad1c670c063b51ce4f8d4463399
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-12 19:44:55 +0900

    Run tests on ResNetVAE configuration changes.

--- tests/test_resnet.py
+++ tests/test_resnet.py
@@ -4,217 +4,165 @@ from omegaconf import DictConfig
 
 from src.configs.model_configs import TrainAutoencoderConfig
 from src.predefined_models import model_define
-from src.predefined_models._CNN_modules import (
-    MyBasicBlock,
-    MyBottleneck,
-    SEBottleneck,
-)
 from src.predefined_models._ResNetVAE import (
     DownSamplingResNet,
     UpSamplingResNet,
-    _calc_layers_output_size,
 )
 
 
 def _load_config() -> DictConfig:
-    with hydra.initialize(
-        "../src/configs/train_conf/test_configs/", version_base=None
-    ):
-        cfg = hydra.compose(config_name="autoencoder")
+    with hydra.initialize("../tests/test_configs/", version_base=None):
+        cfg = hydra.compose(config_name="ResNetVAE")
     return cfg
 
 
-def test_mybasicblock() -> None:
+def test_downsample_mybasicblock() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_size = (32, 32)
+    resolution = 32
     resnet = DownSamplingResNet(
-        MyBasicBlock,
+        "basicblock",
         layers=(3, 4, 6, 3),
-        in_ch=3,
-        out_ch=100,
+        input_channels=3,
+        output_channels=100,
         activation="relu",
+        resolution=resolution,
     ).to(device)
 
-    t = torch.rand(10, 3, *input_size, device=device)
+    t = torch.rand(10, 3, resolution, resolution, device=device)
 
-    output_size_conv1_calc = _calc_layers_output_size(
-        input_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size_calc = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
+    out = resnet.forward(t)
 
-    out, output_size_conv1_act, indices = resnet(t)
-
-    assert output_size_conv1_act == (10, 64, *output_size_conv1_calc)
-    assert indices.shape == (10, 64, *indices_size_calc)
     assert out.shape == (10, 100)
 
 
-def test_mybottleneck() -> None:
+def test_downsample_mybottleneck() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_size = (32, 32)
+    resolution = 32
     resnet = DownSamplingResNet(
-        MyBottleneck,
+        "bottleneck",
         layers=(3, 4, 6, 3),
-        in_ch=3,
-        out_ch=100,
+        input_channels=3,
+        output_channels=100,
         activation="relu",
+        resolution=resolution,
     ).to(device)
 
-    t = torch.rand(10, 3, *input_size, device=device)
+    t = torch.rand(10, 3, resolution, resolution, device=device)
 
-    output_size_conv1_calc = _calc_layers_output_size(
-        input_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size_calc = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
+    out = resnet(t)
 
-    out, output_size_conv1_act, indices = resnet(t)
-
-    assert output_size_conv1_act == (10, 64, *output_size_conv1_calc)
-    assert indices.shape == (10, 64, *indices_size_calc)
     assert out.shape == (10, 100)
 
 
-def test_sebottleneck() -> None:
+def test_downsample_sebottleneck() -> None:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_size = (32, 32)
+    resolution = 32
     resnet = DownSamplingResNet(
-        SEBottleneck,
+        "sebottleneck",
         layers=(3, 4, 6, 3),
-        in_ch=3,
-        out_ch=100,
+        input_channels=3,
+        output_channels=100,
         activation="relu",
+        resolution=resolution,
     ).to(device)
 
-    t = torch.rand(10, 3, *input_size, device=device)
+    t = torch.rand(10, 3, resolution, resolution, device=device)
 
-    output_size_conv1_calc = _calc_layers_output_size(
-        input_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size_calc = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
+    out = resnet(t)
 
-    out, output_size_conv1_act, indices = resnet(t)
-
-    assert output_size_conv1_act == (10, 64, *output_size_conv1_calc)
-    assert indices.shape == (10, 64, *indices_size_calc)
     assert out.shape == (10, 100)
 
 
 def test_upsample_mybasicblock() -> None:
-    upsampled_size = (32, 32)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     t = torch.rand(10, 100, device=device)
-    output_size_conv1_calc = _calc_layers_output_size(
-        upsampled_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
-    indices = torch.randint(0, 10, (10, 64, *indices_size), device=device)
+    resolution = 32
+    dim = 100
+    ch = 3
 
     resnet = UpSamplingResNet(
-        MyBasicBlock,
+        "basicblock",
         layers=(3, 4, 6, 3),
-        in_ch=100,
-        out_ch=1,
+        input_channels=dim,
+        output_channels=ch,
         activation="relu",
-        reconstructed_size=(32, 32),
+        resolution=resolution,
     ).to(device)
     print(resnet)
 
-    out = resnet.forward(t, output_size_conv1_calc, indices)
-    assert out.shape == (10, 1, *upsampled_size)
+    out = resnet.forward(t)
+    assert out.shape == (10, ch, resolution, resolution)
 
 
 def test_upsample_mybottleneck() -> None:
-    upsampled_size = (32, 32)
+    resolution = 32
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     t = torch.rand(10, 100, device=device)
-    output_size_conv1_calc = _calc_layers_output_size(
-        upsampled_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
-    indices = torch.randint(0, 10, (10, 64, *indices_size), device=device)
+    dim = 100
+    ch = 3
 
     resnet = UpSamplingResNet(
-        MyBottleneck,
+        "bottleneck",
         layers=(3, 4, 6, 3),
-        in_ch=100,
-        out_ch=1,
+        input_channels=dim,
+        output_channels=ch,
         activation="relu",
-        reconstructed_size=(32, 32),
+        resolution=resolution,
     ).to(device)
-    # print(resnet)
-    print(f"{output_size_conv1_calc=}")
-    print(f"{indices.shape=}")
 
-    out = resnet.forward(t, output_size_conv1_calc, indices)
-    assert out.shape == (10, 1, *upsampled_size)
+    out = resnet.forward(t)
+    assert out.shape == (10, ch, resolution, resolution)
 
 
 def test_upsample_sebottleneck() -> None:
-    upsampled_size = (32, 32)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     t = torch.rand(10, 100, device=device)
-    output_size_conv1_calc = _calc_layers_output_size(
-        upsampled_size, (7,), (3,), (1,), (2,)
-    )
-    indices_size = _calc_layers_output_size(
-        output_size_conv1_calc, (3,), (1,), (1,), (2,)
-    )
-    indices = torch.randint(0, 10, (10, 64, *indices_size), device=device)
+    resolution = 32
+    dim = 100
+    ch = 3
 
     resnet = UpSamplingResNet(
-        SEBottleneck,
+        "bottleneck",
         layers=(3, 4, 6, 3),
-        in_ch=100,
-        out_ch=1,
+        input_channels=dim,
+        output_channels=ch,
         activation="relu",
-        reconstructed_size=(32, 32),
+        resolution=resolution,
     ).to(device)
-    print(resnet)
 
-    print(f"{output_size_conv1_calc=}")
-    out = resnet.forward(t, output_size_conv1_calc, indices)
-    assert out.shape == (10, 1, *upsampled_size)
+    out = resnet.forward(t)
+    assert out.shape == (10, ch, resolution, resolution)
 
 
 def test_up_down_sampling_with_bottleneck() -> None:
-    block = MyBottleneck
+    block = "bottleneck"
+    resolution = 32
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    input_size = (32, 32)
+    input_channel = 3
+    latent_dimension = 100
 
     downsampling = DownSamplingResNet(
         block,
         layers=(3, 4, 6, 3),
-        in_ch=3,
-        out_ch=100,
+        input_channels=input_channel,
+        output_channels=latent_dimension,
         activation="relu",
+        resolution=resolution,
     ).to(device)
 
     upsampling = UpSamplingResNet(
         block,
         layers=(3, 4, 6, 3),
-        in_ch=100,
-        out_ch=3,
+        input_channels=latent_dimension,
+        output_channels=input_channel,
         activation="relu",
-        reconstructed_size=input_size,
+        resolution=resolution,
     ).to(device)
 
-    t = torch.rand(10, 3, *input_size, device=device)
+    t = torch.rand(10, 3, resolution, resolution, device=device)
 
-    vec, size, indices = downsampling.forward(t)
-    print(f"{vec.shape=}")
-    print(f"{size=}")
-    print(f"{indices.shape=}")
-    mat = upsampling(vec, size, indices)
+    vec = downsampling.forward(t)
+    mat = upsampling.forward(vec)
 
     assert mat.shape == t.shape
 
@@ -234,10 +182,16 @@ def test_load_resnetvae() -> None:
 
 def test_training_resnetvae() -> None:
     cfg = TrainAutoencoderConfig.from_dictconfig(_load_config())
-    print(cfg.model)
+
+    ch = cfg.model.hyper_parameters.input_channels
+    resolution = cfg.model.hyper_parameters.input_resolution
+
+    assert isinstance(ch, int)
+    assert isinstance(resolution, int)
 
     model = model_define(cfg.model)
-    for bs in [16, 32, 64, 128]:
-        x = torch.randn((bs, 1, 32, 32))
-        y = model(x)
-        print(y[0].shape)
+    print(model)
+    dummy = torch.randn(1, ch, resolution, resolution)
+    y = model(dummy)[0]
+    assert y.shape == dummy.shape
+    # torchinfo.summary(model, (1, ch, resolution, resolution))

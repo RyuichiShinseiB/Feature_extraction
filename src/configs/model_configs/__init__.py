5e2ac6b3142fda980a79b630f804e8c0e6e003e9
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-24 15:30:37 +0900

    Changed importings

--- src/configs/model_configs/__init__.py
+++ src/configs/model_configs/__init__.py
@@ -1,13 +1,16 @@
 from .autoencoder_configs import (
     AutoencoderModelConfig,
-    ExtractConfig,
+    # ExtractConfig,
     MAEViTModelConfig,
     TrainAutoencoderConfig,
     TrainAutoencoderConfigV2,
     TrainMAEViTConfig,
     TrainVAEConfig,
+    v1,
+    v2,
 )
 from .base_configs import (
+    ExtractConfig,
     dict2dataclass,
     dictconfig2dataclass,
 )

5f1d50dd522d0021b7db5e153b0beb414e2d9df6
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2024-12-26 15:55:27 +0900

    Added prediction value to dataframe

--- src/running_script/features_extractionv2.py
+++ src/running_script/features_extractionv2.py
@@ -9,25 +9,27 @@ from omegaconf import DictConfig
 from torch.utils.data import DataLoader
 
 from src.configs.model_configs import ExtractConfig
-from src.mytyping import Device, Model, Tensor
+from src.mytyping import Device, Tensor
 from src.predefined_models import LoadModel
 from src.utilities import ForExtractFolder, get_dataloader
 
 
 @torch.no_grad()
 def _extract_features(
-    first_stage: Model,
+    model: torch.nn.Sequential,
     dataloader: DataLoader[ForExtractFolder],
     device: Device,
-) -> tuple[np.ndarray, list[int], list[str], list[str]]:
+) -> tuple[np.ndarray, list[int], list[int], list[str], list[str]]:
     features_list: list[Tensor] = []
+    pred_list: list[int] = []
     target_list: list[int] = []
     dirnames_list: list[str] = []
     filenames_list: list[str] = []
 
-    first_stage.eval()
+    model.eval()
     for x, target, _, dirnames, filenames in tqdm.tqdm(dataloader):
-        features = first_stage(x.to(device))
+        features = model[0](x.to(device))
+        y: Tensor = model[1](features)
         if isinstance(features, tuple):
             if len(features) == 2:
                 features = features[0]
@@ -36,23 +38,36 @@ def _extract_features(
         features_list.append(
             torch.flatten(features, start_dim=1).detach().cpu()
         )
-        target_list.extend(target.detach().cpu().tolist())
+        if y.shape[1] == 1:
+            pred = torch.where(y > 0.5, 1, 0).detach().cpu().flatten().tolist()
+        else:
+            pred = torch.argmax(y).detach().cpu().flatten().tolist()
+        target_list.extend(target)
+        pred_list.extend(pred)
         dirnames_list.extend(dirnames)
         filenames_list.extend(filenames)
     features_array = torch.concat(features_list, dim=0).numpy()
 
-    return features_array, target_list, dirnames_list, filenames_list
+    return (
+        features_array,
+        target_list,
+        pred_list,
+        dirnames_list,
+        filenames_list,
+    )
 
 
 def _sort_out_extracted_data(
     features: np.ndarray,
     targets: list[int],
+    preds: list[int],
     dirnames: list[str],
     filenames: list[str],
 ) -> pl.DataFrame:
     df = pl.DataFrame(features).with_columns(
         [
             pl.lit(pl.Series("target", targets, dtype=pl.Int32)),
+            pl.lit(pl.Series("prediction", preds, dtype=pl.Int32)),
             pl.lit(pl.Series("dirname", dirnames, dtype=pl.Utf8)),
             pl.lit(pl.Series("filename", filenames, dtype=pl.Utf8)),
         ]
@@ -61,14 +76,16 @@ def _sort_out_extracted_data(
 
 
 def _get_feature_table(
-    first_stage: Model,
+    model: torch.nn.Sequential,
     dataloader: DataLoader[ForExtractFolder],
     device: Device,
 ) -> pl.DataFrame:
-    features, targets, dirnames, filenames = _extract_features(
-        first_stage, dataloader, device
+    features, targets, preds, dirnames, filenames = _extract_features(
+        model, dataloader, device
     )
-    df = _sort_out_extracted_data(features, targets, dirnames, filenames)
+    df = _sort_out_extracted_data(
+        features, targets, preds, dirnames, filenames
+    )
     return df
 
 
@@ -83,7 +100,7 @@ def main(_cfg: DictConfig) -> None:
 
     # 抽出した特徴量の保存先
     base_save_path = Path(cfg.train.trained_save_path)
-    model_save_path = "./models" / base_save_path / "model_parameters.pth"
+    model_save_path = "./models" / base_save_path / "model_parameters_ea.pth"
     feature_storing_path = "./reports/features" / base_save_path
     if not feature_storing_path.exists():
         feature_storing_path.mkdir(parents=True)
@@ -106,9 +123,10 @@ def main(_cfg: DictConfig) -> None:
         shuffle=False,
         generator_seed=None,
         extraction=True,
+        cls_conditions=cfg.dataset.cls_conditions,
     )
     print("Extraction from training data...")
-    train_df = _get_feature_table(first_stage, train_dataloader, device)
+    train_df = _get_feature_table(model, train_dataloader, device)
     train_df.select(pl.all().sort_by("filename")).write_csv(
         feature_storing_path / "features_train_data.csv"
     )
@@ -121,9 +139,10 @@ def main(_cfg: DictConfig) -> None:
         shuffle=False,
         generator_seed=None,
         extraction=True,
+        cls_conditions=cfg.dataset.cls_conditions,
     )
     print("Extraction from checking data...")
-    check_df = _get_feature_table(first_stage, check_dataloader, device)
+    check_df = _get_feature_table(model, check_dataloader, device)
     check_df.select(pl.all().sort_by("filename")).write_csv(
         feature_storing_path / "features_check_data.csv"
     )

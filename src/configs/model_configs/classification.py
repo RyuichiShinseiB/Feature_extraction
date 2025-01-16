19a21da20727340e5c210b469abab3f4b1be4338
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2025-01-16 16:10:32 +0900

    Added method overload

--- src/configs/model_configs/classification.py
+++ src/configs/model_configs/classification.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from typing import overload
 
+import torch
 from torch.utils.data import DataLoader
 
-from ...mytyping import Model
+from ...mytyping import Device, Model
 from ...predefined_models._load_model import LoadModel
 from ...utilities import find_project_root, get_dataloader
 from .base_configs import (
@@ -26,11 +28,35 @@ class TrainClassificationModel(RecursiveDataclass):
     train: TrainConfig = TrainConfig()
     dataset: TrainDatasetConfig = TrainDatasetConfig()
 
-    def create_dataset(self, seed: int = 42) -> DataLoader:
+    @property
+    def verbose_interval(self) -> int:
+        return (
+            self.train.train_hyperparameter.epochs
+            // self.train.train_hyperparameter.num_save_reconst_image
+        )
+
+    @overload
+    def create_dataloader(
+        self, split_ratio: None, seed: int = 42
+    ) -> DataLoader:
+        ...
+
+    @overload
+    def create_dataloader(
+        self, split_ratio: tuple[float, float], seed: int = 42
+    ) -> tuple[DataLoader, DataLoader]:
+        ...
+
+    def create_dataloader(
+        self,
+        split_ratio: tuple[float, float] | None = (0.8, 0.2),
+        seed: int = 42,
+    ) -> DataLoader | tuple[DataLoader, DataLoader]:
         root = find_project_root()
         return get_dataloader(
             dataset_path=root / self.dataset.path,
             dataset_transform=self.dataset.transform,
+            split_ratio=split_ratio,
             batch_size=self.train.train_hyperparameter.batch_size,
             generator_seed=seed,
             cls_conditions=self.dataset.cls_conditions,
@@ -40,3 +66,10 @@ class TrainClassificationModel(RecursiveDataclass):
         first_stage = LoadModel.load_model_from_config(self.model.classifier)
         second_stage = LoadModel.load_model_from_config(self.model.feature)
         return first_stage, second_stage
+
+    def create_sequential_model(
+        self, device: Device = "cpu"
+    ) -> torch.nn.Sequential:
+        stages = self.create_model()
+        model = torch.nn.Sequential(stages[0], stages[1]).to(device)
+        return model

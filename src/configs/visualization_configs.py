ab5a0729950ff4061097e15e1548fd901ae577f4
Ryuichi Shinsei <275051h@gs.kochi-tech.ac.jp>
2025-01-20 15:01:03 +0900

    Added a class to search by date/time and display its configurations when visualizing features

--- src/configs/visualization_configs.py
+++ src/configs/visualization_configs.py
@@ -1,7 +1,14 @@
+import datetime
 from dataclasses import dataclass
 from pathlib import Path
 
+from hydra import compose, initialize
 from omegaconf import DictConfig, OmegaConf
+
+from src.configs.model_configs import ExtractConfig
+from src.utilities import find_project_root
+
+# from ..utilities import find_project_root
 
 
 @dataclass
@@ -50,3 +57,46 @@ class VisConfigs:
         ecfg = ExtractionModelConfig(**_cfg["extraction"])
         ccfg = ClusteringModelConfig(**_cfg["clustering"])
         return cls(ecfg, ccfg)
+
+
+@dataclass
+class FeatureDatetimeCfg:
+    running_date: datetime.date
+    running_time: datetime.time
+
+    def running_datetime(self, f: str = "%Y-%m-%d/%H-%M-%S") -> str:
+        dt = datetime.datetime(
+            self.running_date.year,
+            self.running_date.month,
+            self.running_date.day,
+            self.running_time.hour,
+            self.running_time.minute,
+            self.running_time.second,
+        )
+        return dt.strftime(f)
+
+    def __post_init__(self) -> None:
+        run_datetime = self.running_datetime()
+        root = find_project_root(relative=True)
+        feature_dir = root / "reports/features"
+        res = list(feature_dir.glob(f"*/{run_datetime}/.hydra"))
+        if res == []:
+            raise FileNotFoundError(
+                "Could not find the directory at ",
+                "the specified run date and time.\n",
+                f": `{self.running_datetime('%Y/%m/%d %H:%M:%S')}`",
+            )
+
+        print(res[0])
+        print(res[0].exists())
+        with initialize(str(".." / res[0]), version_base=None):
+            _cfg = compose("config")
+        self.extract_cfg = ExtractConfig.from_dictconfig(_cfg)
+
+    @classmethod
+    def from_datetime(
+        cls, year: int, month: int, day: int, hour: int, minute: int, sec: int
+    ) -> "FeatureDatetimeCfg":
+        return cls(
+            datetime.date(year, month, day), datetime.time(hour, minute, sec)
+        )
